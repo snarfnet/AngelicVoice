38730797bf044fc0a95b2e560ac494d030eb7c2f
snarfnet <snarfnet@users.noreply.github.com>
2026-08-01 07:17:57 +0900

    Set ASC APP_ID 6796853280 for TestFlight submission

--- scripts/submit.py
+++ scripts/submit.py
@@ -2,7 +2,7 @@ import jwt, time, requests, sys
 
 KEY_ID       = 'WDXGY9WX55'
 ISSUER       = '2be0734f-943a-4d61-9dc9-5d9045c46fec'
-APP_ID       = 'TODO_APP_ID'
+APP_ID       = '6796853280'
 BUILD_NUMBER = sys.argv[1]
 
 if APP_ID.startswith('TODO'):
